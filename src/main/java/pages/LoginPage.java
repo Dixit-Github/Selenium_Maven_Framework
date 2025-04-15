3f4ec3d2338cc6d5c9fa98e8e11753e65cc39632
Dixit-Github <nagraj.s.dixit@gmail.com>
2025-04-15 20:14:20 +0530

    commit for 15-April

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -2,37 +2,63 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import utils.Log;
 
 public class LoginPage {
 	
 	private WebDriver driver;
 	
-	//WebElements on Login page 
-	private  By usernameTextBox = By.xpath("//input[@id='Email']");
-	private By passwordTextBox = By.xpath("//input[@id='Password']");
-	private By loginButton = By.xpath("//button[@type='submit']");
+	//Locating WebElements on Login page 
+	/*
+	 * private By usernameTextBox = By.xpath("//input[@id='Email']"); 
+	 * private By passwordTextBox = By.xpath("//input[@id='Password']"); 
+	 * private By loginButton = By.xpath("//button[@type='submit']");
+	 */
+	
+	
+	//Locating WebElements using PageFactory
+	@FindBy (id= "Email") 
+	WebElement usernameTextBox;
+	
+	@FindBy (id= "Password") 
+	WebElement passwordTextBox;
+	
+	@FindBy (xpath= "//button[@type='submit']") 
+	WebElement loginButton;
 	
 	public LoginPage (WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 	
 	//Action methods/functions for WebElements 
 	public void enterUsername (String username) throws InterruptedException {
-		driver.findElement(usernameTextBox).clear();
-		Thread.sleep(1000);
-		driver.findElement(usernameTextBox).sendKeys(username);
+	//	driver.findElement(usernameTextBox).clear();
+		usernameTextBox.clear();
+		
+	//	driver.findElement(usernameTextBox).sendKeys(username);
+		usernameTextBox.sendKeys(username);
+		
 		Thread.sleep(1000);
 	}
 	
 	public void enterPassword(String password) throws InterruptedException {
-		driver.findElement(passwordTextBox).clear();
-		Thread.sleep(1000);
-		driver.findElement(passwordTextBox).sendKeys(password);
+	//	driver.findElement(passwordTextBox).clear();
+		passwordTextBox.clear();
+				
+	//	driver.findElement(passwordTextBox).sendKeys(password);
+		passwordTextBox.sendKeys(password);
 		Thread.sleep(1000);
 	}
 	
 	public void clickLogin () throws InterruptedException {
-		driver.findElement(loginButton).click();
+		Log.info("Clicking login button.");
+	//	driver.findElement(loginButton).click();
+		loginButton.click();
 		Thread.sleep(1000);
 	}
 	
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,22 +1,95 @@
 package tests;
 
+import java.io.IOException;
+
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExcelUtils;
+import utils.ExtentReportManager;
+import utils.Log;
 
 public class LoginTest extends BaseTest {
 
-	@Test
-	public void testValidLogin() throws InterruptedException {
-		LoginPage loginPage = new LoginPage(driver);
-		loginPage.enterUsername("admin@yourstore.com");
-		loginPage.enterPassword("admin");
-		loginPage.clickLogin();
-		
-		System.out.println("Title of page: "+driver.getTitle());
-		Thread.sleep(2000);
-		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+	@DataProvider(name = "LoginData")
+	public Object[][] getLoginData() throws IOException {
+		String filePath = System.getProperty("user.dir") + "/testdata/TestData.xlsx";
+		ExcelUtils.loadExcel(filePath, "Sheet1");
+		int rowCount = ExcelUtils.getRowCount();
+		Object[][] data = new Object[rowCount - 1][2];
+
+		for (int i = 1; i < rowCount; i++) {
+			data[i - 1][0] = ExcelUtils.getCellData(i, 0); // Username
+			data[i - 1][1] = ExcelUtils.getCellData(i, 1); // Password
+		}
+		ExcelUtils.closeExcel();
+		return data;
 	}
 
+//	@Test (dataProvider = "LoginData")
+
+	@Test
+	@Parameters({ "username", "password" }) // taking form testng.xml
+	public void testValidLogin(String username, String password) throws InterruptedException {
+
+		Log.info("Starting valid login test...");
+		test = ExtentReportManager.createTest("Login Test -" + username);
+
+		test.info("Navigating to URL.");
+		LoginPage loginPage = new LoginPage(driver);
+
+		Log.info("Entering valid username and password.");
+		test.info("Adding correct credentials.");
+
+		// loginPage.enterUsername("admin@yourstore.com");
+		// loginPage.enterPassword("admin");
+
+		// DataProvider username and password
+		loginPage.enterUsername(username);
+		loginPage.enterPassword(password);
+
+		test.info("Clicking on Login button.");
+		loginPage.clickLogin();
+
+		System.out.println("Title of page: " + driver.getTitle());
+		Thread.sleep(1000);
+
+		Log.info("Verifying page title...");
+		test.info("Verifying page title.");
+		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+
+		test.pass("Login Successful.");
+		Log.info("Login test completed.");
+	}
+
+	@Test(enabled = false)
+	public void testInvalidLogin() throws InterruptedException {
+
+		Log.info("Starting invalid login test...");
+		test = ExtentReportManager.createTest("Login Test");
+
+		test.info("Navigating to URL.");
+		LoginPage loginPage = new LoginPage(driver);
+
+		Log.info("Entering invalid username and password.");
+		test.info("Adding incorrect credentials.");
+		loginPage.enterUsername("admin123@yourstore.com");
+		loginPage.enterPassword("admin123");
+
+		test.info("Clicking on Login button.");
+		loginPage.clickLogin();
+
+		System.out.println("Title of page: " + driver.getTitle());
+		Thread.sleep(1000);
+
+		Log.info("Verifying page title...");
+		test.info("Verifying page title.");
+		Assert.assertEquals(driver.getTitle(), "Just a moment...123");
+
+		test.pass("Login Successful.");
+		Log.info("Login test completed.");
+	}
 }
